572297ffc72efa032470c1574cb5d9bfa6b8dfee
Eunice Dhivya <divi.seed@gmail.com>
2021-09-18 01:53:30 +0530

    Update assignment-day3.js

--- assignment-day3.js
+++ assignment-day3.js
@@ -2,25 +2,23 @@
 // =============================================================
 
 // Set up our HTTP request
-// var dataForLoop = null;
-var xhr = new XMLHttpRequest();
 
-// Setup our listener to process completed requests
+let xhr = new XMLHttpRequest();
+
+
 xhr.onload = function () {
-  // Process our return data
+
   if (xhr.status >= 200 && xhr.status < 300) {
-    // Runs when the request is successful
-    var t = JSON.parse(this.response);
-    forLoop(t)
+
+    let data = JSON.parse(this.response);
+    forLoop(data)
   } else {
-    // Runs when it's not
+
     console.log(xhr.responseText);
   }
 };
 
 // Create and send a GET request
-// The first argument is the post type (GET, POST, PUT, DELETE, etc.)
-// The second argument is the endpoint URL
 xhr.open("GET", "https://restcountries.eu/rest/v2/all");
 xhr.send();
 
